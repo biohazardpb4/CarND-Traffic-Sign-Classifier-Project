2aacc5b398444cb8f27c892159c47553cf1dc2b7
biohazarpb4 <carnd@ip-172-31-90-143.ec2.internal>
2018-02-26 01:59:45 +0000

    switches fetched data set to one supplied by Udacity instead of one supplied by GTSRB

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,7 +2,6 @@
 
 mkdir data
 cd data
-wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Training_Images.zip
-unzip GTSRB_Final_Training_Images.zip
-wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_Images.zip
-unzip GTSRB_Final_Test_Images.zip
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
+unzip traffic-signs-data.zip
+
